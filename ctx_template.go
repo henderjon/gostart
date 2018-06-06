32d73014800e55c16ff26dbea302c6b268453930
henderjon <list@box370.net>
2018-06-06 14:13:48 -0500

    adds template for contexts

---
+++ ctx_template.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+)
+
+type ctxTmpl struct {
+	Val int
+}
+
+// custom type to use as a key to avoid collisions within the context
+type ctxKeyTmpl int
+
+// initialize a value of the custom type to avoid collisions within the context
+var ctxTmplKey ctxKeyTmpl
+
+// function to set a *ctxTmpl at our custom typed key
+func ctxSetTmpl(ctx context.Context, b *ctxTmpl) context.Context {
+	return context.WithValue(ctx, ctxTmplKey, b)
+}
+
+// func type to use as psuedo-interface in external applications
+type ctxTmplGetter func(ctx context.Context) (*ctxTmpl, bool)
+
+// function to get a *ctxTmpl at our custom typed key
+func ctxGetTmpl(ctx context.Context) (*ctxTmpl, bool) {
+	b, ok := ctx.Value(ctxTmplKey).(*ctxTmpl)
+	return b, ok
+}
